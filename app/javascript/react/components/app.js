27c4fbb9be0b235870cb1499712fe280f4becbbf
Ethan Yu-Sheng Chang <ethanysc@github.com>
2018-08-07 13:24:43 -0400

    reformat App.js to a router

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Route, IndexRoute, Router, browserHistory } from 'react-router';
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return (
+    <Router history={browserHistory} >
+      <Route path='/' >
+        <IndexRoute component={} />
+      </Route>
+    </Router>
+  )
 }
 
 export default App
