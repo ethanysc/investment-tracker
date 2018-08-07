1e82b03fc58aac9233c153b284f3007f673ea102
Ethan Yu-Sheng Chang <ethanysc@github.com>
2018-08-07 13:52:28 -0400

    Yarn install react-router, setup portfolio main page and mvc

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Route, IndexRoute, Router, browserHistory } from 'react-router';
+import { Route, IndexRoute, Router, browserHistory } from 'react-router'
+
+import PortfolioIndexContainer from '../containers/PortfolioIndexContainer'
 
 export const App = (props) => {
   return (
     <Router history={browserHistory} >
       <Route path='/' >
-        <IndexRoute component={} />
+        <IndexRoute component={PortfolioIndexContainer}/>
       </Route>
     </Router>
   )
