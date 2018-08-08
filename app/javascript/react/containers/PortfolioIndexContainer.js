d677d0ff0a790973391f00ece0ac801b35968429
Ethan Yu-Sheng Chang <ethanysc@github.com>
2018-08-08 00:28:25 -0400

    Add empty links to format top bar in rails (layout/application.html.erb)

--- app/javascript/react/containers/PortfolioIndexContainer.js
+++ app/javascript/react/containers/PortfolioIndexContainer.js
@@ -4,11 +4,28 @@ class PortfolioIndexContainer extends React.Component {
   constructor(props){
     super(props)
     this.state = {}
+
+    this.timeOfDay = this.timeOfDay.bind(this)
+  }
+
+  timeOfDay(){
+    let currentTime = new Date()
+    if (currentTime < 12){
+      return 'Good Morning, '
+    }
+    else if (currentTime < 18){
+      return 'Good Afternoon, '
+    }
+    else{
+      return 'Good Evening, '
+    }
   }
 
   render(){
     return(
-      <div>PortfolioIndexContainer</div>
+      <div className='porfolio-index-wrapper'>
+        <h1>{this.timeOfDay()}Welcome to InvestmentTracker</h1>
+      </div>
     )
   }
 }
