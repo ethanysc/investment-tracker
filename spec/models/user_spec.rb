af475794017a6e363d40c59a26e81fd9c4d49de6
Ethan Yu-Sheng Chang <ethanysc@github.com>
2018-08-26 13:21:49 -0400

    User model validation + associations + class methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,29 +10,55 @@ RSpec.describe User, type: :model do
     it "is valid with all required fields specified" do
       expect(user).to be_valid
     end
+
     it "is invalid with username not specified" do
       expect(user1).not_to be_valid
     end
+
     it "is invalid with e-mail not specified" do
       expect(user2).not_to be_valid
     end
+
     it "is invalid with password not specified" do
       expect(user3).not_to be_valid
     end
   end
 
-  # describe "#search_stock" do
-  #   let!(:user) {FactoryBot.build(:user)}
-  #   let!(:sector) {FactoryBot.build(:sector)}
-  #   let!(:stock) {FactoryBot.build(:stock)}
-  #   let!(:stock_ownership) {FactoryBot.build(:stock_ownership)}
-  #
-  #   it "returns correct stock_ownership record" do
-  #     stock.sector = sector
-  #     stock_ownership.user = user
-  #     stock_ownership.stock = stock
-  #
-  #     expect(user.search_stock(stock)).to equal stock_ownership
-  #   end
-  # end
+  describe "ActiveRecord associations" do
+    let!(:user) {FactoryBot.build(:user)}
+
+    it "has many stock_ownerships" do
+      expect(user).to have_many(:stock_ownerships)
+    end
+
+    it "has many stocks" do
+      expect(user).to have_many(:stocks).through(:stock_ownerships)
+    end
+
+    it "has many stock_ownerships" do
+      expect(user).to have_many(:reviews)
+    end
+  end
+
+  describe "#search_stock" do
+    let!(:user) {FactoryBot.create(:user, id: 1)}
+    let!(:sector) {FactoryBot.create(:sector, id: 1)}
+    let!(:stock) {FactoryBot.create(:stock, id: 1)}
+    let!(:stock_ownership) {FactoryBot.create(:stock_ownership)}
+
+    it "returns correct stock_ownership record" do
+      expect(user.search_stock(stock)).to eq stock_ownership
+    end
+  end
+
+  describe "#has_stock?" do
+    let!(:user) {FactoryBot.create(:user, id: 1)}
+    let!(:sector) {FactoryBot.create(:sector, id: 1)}
+    let!(:stock) {FactoryBot.create(:stock, id: 1)}
+    let!(:stock_ownership) {FactoryBot.create(:stock_ownership)}
+
+    it "checks if user owns certain stock" do
+      expect(user.has_stock?(stock)).to eq true
+    end
+  end
 end
